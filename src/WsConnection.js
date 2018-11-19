e144ce10d31360f62556d64f75209e6a7b930520
Thomas Hunter <th@thomashunter.co.uk>
2018-11-19 23:08:22 +0000

    Application relays websocket client and server, outputing what is happening to the console.

--- src/WsConnection.js
+++ src/WsConnection.js
@@ -1,27 +1,61 @@
+import { client as WsClient } from 'websocket';
 
 let connectionIdCounter = 0;
 
 class WsConneciton {
     constructor(request) {
-        this._message.bind(this);
-        this._close.bind(this);
+        this._handleClientMessage.bind(this);
+        this._handleClientClose.bind(this);
+        this._handleServerMessage.bind(this);
+        this._handleServerClose.bind(this);
         this._log.bind(this);
 
         this.connectionId = connectionIdCounter++;
 
         this._log('Client connected');
 
-        const connection = request.accept(null, request.origin);
-        connection.on('message', message => this._message(message));
-        connection.on('close', () => this._close());
+        this.wsClient = new WsClient();
+
+        this.wsClient.on('connect', (connection) => {
+            this._log('Server Connected');
+            this.wsClientConnection = connection;
+            this.wsClientConnection.on('message', (message) => this._handleServerMessage(message));
+            this.wsClientConnection.on('close', () => this._handleServerClose());
+        });
+        this.wsClient.on('connectFailed', () => {
+            this._log(`Failed to relay connection to ${process.env.CONNECT_ADDR}`);
+            this.connection.close();
+        });
+
+        this.wsClient.connect(process.env.CONNECT_ADDR);
+
+        this.connection = request.accept(null, request.origin);
+        this.connection.on('message', message => this._handleClientMessage(message));
+        this.connection.on('close', () => this._handleClientClose());
     }
 
-    _message(message) {
-        this._log('Client Sent', message);
+    _handleClientMessage(message) {
+        if (message.type === 'utf8') {
+            this._log('Client Sent', message.utf8Data);
+            this.wsClientConnection.sendUTF(message.utf8Data);
+        }
     }
     
-    _close() {
+    _handleClientClose() {
         this._log('Client disconnected');
+        this.wsClientConnection.close();
+    }
+
+    _handleServerMessage(message) {
+        if (message.type === 'utf8') {
+            this._log('Server Sent', message.utf8Data);
+            this.connection.sendUTF(message.utf8Data);
+        }
+    }
+
+    _handleServerClose() {
+        this._log('Server disconnected');
+        this.connection.close();
     }
 
     _log(message, ...optionalParams) {
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
+import { load as DotEnvLoad } from 'dotenv';
 import { server as WsServer } from 'websocket';
 import HttpServer from 'http';
 import WsConneciton from './WsConnection';
 
-
-const serverPort = 8081;
-
-
+DotEnvLoad();
 
 const server = HttpServer.createServer((request, response) => {
 
 });
 
-server.listen(serverPort, () => {});
+const ServerPort = process.env.SERVER_PORT || 8081;
+
+server.listen(ServerPort, () => {});
 
 let wsServer = new WsServer({
     httpServer: server
 });
 
 wsServer.on('request', (request) => new WsConneciton(request));
+
+console.log(`Server listening on port ${ServerPort}`);
